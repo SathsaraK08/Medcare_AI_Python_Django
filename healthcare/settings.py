8f3389526be1ba9f18b18506c0cdf6101d626332
Sathsara Karunarathne <79186512+RomiS08@users.noreply.github.com>
2023-09-24 06:03:45 +0530

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -116,6 +116,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "core/static"),)
 
 
